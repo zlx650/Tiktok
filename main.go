5089880bbba52c5aa25fa82624889151a38aedf6
淮0 <0>
2023-08-10 15:46:53 +0800

    test commit

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+  if err := r.Run(); err != nil{
+    return 
+  }
 }
