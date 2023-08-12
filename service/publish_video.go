d645a574cbeb21cec8cacd65ad3a4940d2a06b23
XIAOJIANG LIN <1227891082@qq.com>
2023-08-12 16:44:31 +0800

    publish_video first version

---
+++ service/publish_video.go
@@ -0,0 +1,53 @@
+package service
+
+import (
+	"tiktok/util"
+)
+
+type PublishVideoFlow struct {
+	videoName string
+	coverName string
+	title     string
+	authorId  int64
+
+	video *util.Video
+}
+
+func (f *PublishVideoFlow) Do() error {
+	if err := f.checkParam(); err != nil {
+		return err
+	}
+	if err := f.publish(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *PublishVideoFlow) checkParam() error {
+	f.videoName = util.GetDataUrl(f.videoName)
+	f.coverName = util.GetDataUrl(f.coverName)
+	return nil
+}
+
+func (f *PublishVideoFlow) publish() error {
+	video := &util.Video{
+		AuthorId: f.authorId,
+		PlayUrl:  f.videoName,
+		CoverUrl: f.coverName,
+		Title:    f.title,
+	}
+	return util.NewVideoDaoInstance().CreateVideo(video)
+}
+
+func PublishVideo(userId int64, videoName, coverName, title string) error {
+	return NewPublishVideoFlow(userId, videoName, coverName, title).Do()
+}
+
+func NewPublishVideoFlow(userId int64, videoName, coverName, title string) *PublishVideoFlow {
+	return &PublishVideoFlow{
+		authorId:  userId,
+		videoName: videoName,
+		coverName: coverName,
+		title:     title,
+	}
+}
